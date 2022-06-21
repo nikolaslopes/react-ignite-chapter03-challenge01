afc8e47a3ea7fa51f8e5f9f9481434fc802cf370
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-21 00:15:24 -0400

    :bug: fix: header element

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import styles from './header.module.scss';
 
-export default function Header() {
+export default function Header(): JSX.Element {
   return (
     <header className={styles.headerContent}>
       <Link href="/">
